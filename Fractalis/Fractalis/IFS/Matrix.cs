6a0481dc457f9e34d324ac0a2341e63680540a7d
sergey.ignatievskiy <sergey.ignatievskiy@gmail.com>
2011-04-20 07:12:04 +0000

    +Тест трансляции отображений

--- Fractalis/Fractalis/IFS/Matrix.cs
+++ Fractalis/Fractalis/IFS/Matrix.cs
@@ -12,8 +12,14 @@ namespace Fractalis.IFS
 
         public Matrix(int dim, double[,] values)
         {
-            this.Dim = dim;
+            Dim = dim;
             this.values = values;
+        }
+
+        public Matrix(int dim)
+        {
+            Dim = dim;
+            values = new double[dim,dim];
         }
 
         public double this[int x, int y]
@@ -34,6 +40,81 @@ namespace Fractalis.IFS
                 }
                 values[x, y] = value;
             }
+        }
+
+        public Matrix Add(Matrix other)
+        {
+            if (other.Dim != Dim)
+            {
+                throw new InvalidOperationException("Размерности матриц не совпадают");
+            }
+            for (int i = 0; i < Dim; i++)
+            {
+                for (int j = 0; j < Dim; j++)
+                {
+                    values[i, j] += other[i, j];
+                }
+            }
+            return this;
+        }
+
+        public Matrix Sub(Matrix other)
+        {
+            if (other.Dim != Dim)
+            {
+                throw new InvalidOperationException("Размерности матриц не совпадают");
+            }
+            for (int i = 0; i < Dim; i++)
+            {
+                for (int j = 0; j < Dim; j++)
+                {
+                    values[i, j] -= other[i, j];
+                }
+            }
+            return this;
+        }
+
+        public Matrix Mul(Double k)
+        {
+            for (int i = 0; i < Dim; i++)
+            {
+                for (int j = 0; j < Dim; j++)
+                {
+                    values[i, j] *= k;
+                }
+            }
+            return this;
+        }
+
+        public Matrix Mul(Matrix other)
+        {
+            if (other.Dim != Dim)
+            {
+                throw new InvalidOperationException("Размерности матриц не совпадают");
+            }
+            var C = new Matrix(Dim);
+            for (int i = 0; i < Dim; i++)
+            {
+                for (int j = 0; j < Dim; j++)
+                {
+                    double sum = 0;
+                    for (int k = 0; k < Dim; k++)
+                    {
+                        sum += values[i, k]*other[k, j];
+                    }
+                    C[i, j] = sum;
+                }
+            }
+
+            for (int i = 0; i < Dim; i++)
+            {
+                for (int j = 0; j < Dim; j++)
+                {
+                    values[i, j] = C[i, j];
+                }
+            }
+            return this;
+
         }
     }
 }
